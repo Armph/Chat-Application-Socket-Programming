e36c5ff9023a061a7404f7d6fdbfd77dc2fe52fa
MisterStank <chakkritchongkraichak@gmail.com>
2024-04-20 15:37:14 +0700

    feat(chat): :sparkles: chat ui

---
+++ frontend/src/components/ChatItem.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Avatar from "./Avatar";
+
+export default function ChatItem({ setChat }) {
+  return (
+    <div className="chat-item" onClick={() => setChat(true)}>
+      <Avatar src="" height={55} width={55} />
+      <div className="chat-item-infos">
+        <div className="avatar-infos">
+          <span className="username">Name Surname</span>
+          <span className="timeline">2 days ago</span>
+        </div>
+        <p className="last-message">Say hi!</p>
+      </div>
+    </div>
+  );
+}
