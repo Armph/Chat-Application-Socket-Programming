d74905d428290441b8d0538ae9c81eafb40ddfbb
Worameth Suwannapruuk <nut.worameth@gmail.com>
2024-04-24 02:05:27 +0700

    nut fix

--- frontend/src/pages/Messenger.jsx
+++ frontend/src/pages/Messenger.jsx
@@ -12,17 +12,10 @@ export default function Messenger() {
     setChat(false);
   }
 
-  const [btn ,setBtn] = useState(false);
-
-  const handleBtn = () => {
-    setBtn(!btn);
-    setChat(false);
-  }
-
   return (
     <div className="messenger">
       <Sidebar setChat={setChat} handleBtn ={handleBtn} setBtn={setBtn} btn = {btn}/>
-      <Content chat={chat} setChat={setChat} btn ={btn} serBtn={setBtn}/>
+      <Content chat={chat} setChat={setChat} btn ={btn} setBtn={setBtn}/>
     </div>
   );
 }
