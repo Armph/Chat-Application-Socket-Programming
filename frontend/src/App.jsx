4e01e6c8acc7b758f06a5698ccd5358b5a51a68d
armph <arrmph@gmail.com>
2024-04-24 01:42:17 +0700

    Hot Fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,16 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Messenger from "./pages/Messenger";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import socketIOClient from 'socket.io-client';
+import { useEffect } from "react";
+import io from 'socket.io-client';
+import { Button } from "@mui/material";
+
+const ENDPOINT = 'http://192.168.1.6:5000';
+const socket = io(ENDPOINT);
 
 export default function App() {
+
   return (
     <BrowserRouter>
       <Routes>
@@ -12,6 +20,7 @@ export default function App() {
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
+      <Button onClick={() => socket.emit('test', 'Hello from React!')}>Test</Button>
     </BrowserRouter>
   );
 }
