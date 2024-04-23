bf97bf6f66b804fbf7f66154a494af3d2f8c6832
Worameth Suwannapruuk <nut.worameth@gmail.com>
2024-04-24 01:40:46 +0700

    fix

--- frontend/src/components/CreateGroupchat.jsx
+++ frontend/src/components/CreateGroupchat.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "../assets/css/creategroupchat.css"
 
+
+
 export default function CreateGroupChat(setChat , setBtn) {
+
+    const [chatName,setChatName] = useState('');
+    const handleConfirm = () => {
+        console.log(chatName);
+    }
+
     return (
         <div className="wrapper">
             <div className="top">
@@ -9,13 +17,13 @@ export default function CreateGroupChat(setChat , setBtn) {
             </div>
             <div className="center">
                 <div className="label"> Chat name </div>
-                <input type="text" placeholder="Your chat name.."></input>
+                <input type="text" placeholder="Your chat name.." value={chatName} onChange = {(e) => setChatName(e.target.value)}></input>
                 <div className="label"> Chat photo </div>
                 <div className="label"> Member </div>
             </div>
             <div className="bottom">
                 <button className="button" onClick={() => {  }}>Cancel</button>
-                <button className="button">Confirm</button>
+                <button className="button" onClick={() => {handleConfirm()}}>Confirm</button>
             </div>
         </div>
     );
