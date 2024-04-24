9bb11d52720c81e85428efad884bc4e0c8e5bba6
Worameth Suwannapruuk <nut.worameth@gmail.com>
2024-04-24 07:50:57 +0700

    nutfinal

--- frontend/src/components/CreateGroupchat.jsx
+++ frontend/src/components/CreateGroupchat.jsx
@@ -16,7 +16,7 @@ export default function CreateGroupChat({setChat , setBtn} ) {
             <div className="center">
                 <div className="label" id="1"> Chat name </div>
                 <input type="text" id="1" placeholder="Your chat name.." value={chatName} onChange = {(e) => setChatName(e.target.value)}></input>
-                <div className="label" id="1"> Chat photo </div>
+                {/* <div className="label" id="1"> Chat photo </div>
                 <div className="avatar-wrapper" id="1">
                     <Avatar height={150} width={150} />
                      <i className="fa-solid fa-camera"></i>
@@ -28,7 +28,7 @@ export default function CreateGroupChat({setChat , setBtn} ) {
                             <ContactItem setChat={setChat} key={index} />
                         ))}
                     </div>
-                </div>
+                </div> */}
             </div>
             <div className="bottom">
                 <button className="button" onClick={() => { setBtn(false);setChat(false); }}>Cancel</button>
--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,11 +1,12 @@
+import { color } from "@mui/system";
 import "../assets/css/message.css";
 
-export default function Message({ owner, msg, backgroundColor}) {
+export default function Message({ owner, msg, backgroundColor,fcolor}) {
   return (
     <div className={owner ? "message owner" : "message"}>
       <span className="name">Name</span>
       <div className="message-wrapper" style={{backgroundColor: backgroundColor}}>
-        <p>{msg}</p>
+        <p style={{color:fcolor}}>{msg}</p>
       </div>
     </div>
   );
